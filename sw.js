61ce8af18e6daab9af5dc45567b3cd8fa5293f47
mardenyflor <admin@scsexport.com>
2025-10-22 13:51:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='famtime-v2';
+const C='famtime-green-v1';
 const A=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))) });
